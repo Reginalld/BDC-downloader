0ed257f823ee3680b01ac18a01e0b0782f8ad267
Regi <reginaldosgomesj@gmail.com>
2025-11-26 11:39:12 -0300

    Adicionado novas colunas

--- brazil_data_cube/brazil_data_cube/api/models/models_db.py
+++ brazil_data_cube/brazil_data_cube/api/models/models_db.py
@@ -1,12 +1,35 @@
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Date, Float, DateTime, func
+from geoalchemy2 import Geometry
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = "users"
+   __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+
+   id = Column(Integer, primary_key=True, index=True)
+   username = Column(String, unique=True, index=True, nullable=False)
+   hashed_password = Column(String, nullable=False)
+
+
+class SatelliteScene(Base):
+   __tablename__ = 'satellite_scenes'
+
+
+   id = Column(Integer, primary_key=True, index=True)
+   filename = Column(String, unique=True, nullable=False)
+  
+   mission = Column(String, nullable=False, index=True)
+   satellite = Column(String, nullable=True)
+   tile_id = Column(String, index=True)
+   date = Column(Date, nullable=False, index=True) 
+   minio_path = Column(String, nullable=False)
+   band = Column(String, index=True, nullable=True)
+  
+   geometry = Column(Geometry('POLYGON', srid=4326))
+  
+   created_at = Column(DateTime(timezone=True), server_default=func.now())
+   edited_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
